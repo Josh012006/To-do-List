da8dc1d66417820cb2d676437adfd2df3792199e
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2025-01-02 20:46:12 -0500

    Finished the app

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,132 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <h3>Ajouter une nouvelle tâche</h3>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <form @submit.prevent="addTask" class="addTask">
+        <input type="text" id="newTask" name="newTask" v-model="newTask" placeholder="Nouvelle tâche">
+        <button class="addButton" :disabled="newTask.length === 0">Ajouter</button>
+    </form>
+
+    <label id="maskLabel" for="mask"><input type="checkbox" name="mask" id="mask" v-model="mask">Masquer les tâches terminées</label>
+
+    <div class="tasks">
+        <p v-if="tasks.length == 0" class="noTask">Aucune tâche à compléter</p>
+
+        <ul v-if="tasks.filter(task => !task.completed).length != 0">
+            <h3>Tâches non complétés</h3>
+            <li :key="`${task.date}`" v-for="task in tasks.filter(task => !task.completed)">
+                <span>{{ task.title }}</span>
+                <span>{{ new Date(task.date).toLocaleDateString() }} {{ new Date(task.date).toLocaleTimeString() }}</span>
+                <button @click="completedTask(task.title, task.date)">✅</button>
+            </li>
+        </ul>
+
+        <hr v-if="tasks.length != 0">
+
+        <ul v-if="tasks.filter(task => task.completed).length != 0 && !mask">
+            <h3>Tâches complétées</h3>
+            <li :key="`${task.date}`" v-for="task in tasks.filter(task => task.completed)">
+                <span class="line-through">{{ task.title }}</span>
+                <span>{{ new Date(task.date).toLocaleDateString() }}  {{ new Date(task.date).toLocaleTimeString() }}</span>
+            </li>
+        </ul>
+
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script lang="ts" setup>
+
+import {ref} from 'vue';
+
+const newTask = ref('');
+const tasks = ref([]);
+const mask = ref(false);
+
+const addTask = () => {
+
+    if(newTask.value !== '') {
+
+        // Ajouter la nouvelle tâche au tableau
+        tasks.value.push(
+            {
+                title: newTask.value,
+                completed: false,
+                date: Date.now()
+            }
+        )
+
+        // Réinitialiser le champ
+        newTask.value = '';
+
+    }
+
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+
+const completedTask = (title:string, date:number) => {
+    tasks.value.map((task) => {
+        if(task.title === title && task.date === date) {
+            task.completed = true;
+        }
+    })
 }
 
-@media (min-width: 1024px) {
-  header {
+</script>
+
+<style>
+
+body {
+    text-align: center;
+}
+
+li {
+    text-align: left;
+}
+
+li span {
+    margin: 10px;
+}
+
+.addTask {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    flex-direction: column;
+    align-content: center;
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    gap: 10px;
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    max-width: 30%;
+
+    margin: auto;
 }
+
+#newTask {
+    border-radius: 10px;
+
+    padding: 10px;
+}
+
+.tasks {
+    margin: 100px;
+}
+
+button {
+    border-radius: 7px;
+
+    padding: 5px;
+    margin: auto;
+
+    max-width: 100px;
+
+    cursor: pointer;
+}
+
+.line-through {
+    text-decoration: line-through;
+}
+
+#maskLabel {
+    margin: 50px;
+    display: block;
+}
+
 </style>
